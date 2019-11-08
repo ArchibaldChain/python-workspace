e414992cec81df6c9798ea714b4ffdd5607cd899
Archibald Chain <1156618773@qq.com>
2019-11-08 14:51:08 +0800

    Finished FrozenLake.py FrozenLake.ipynb is not complete Add modified Euler method in Euler_method

--- Workspace for Python/Reinforcement-learning/FrozenLake.py
+++ Workspace for Python/Reinforcement-learning/FrozenLake.py
@@ -13,11 +13,22 @@ import numpy as np
 import gym
 import random
 import time
-import IPython.display as clear_output
+import IPython.display as display
 
+
+# %% [markdown]
+# ### Creating the environment
+# We will using the environment *FrozenLake-v0*
 
 # %%
 env = gym.make("FrozenLake-v0")
+
+# %% [markdown]
+# ### Creating the Q-table
+# We are going to construct our Q-table, and initialize all the Q-values.
+# number of rows in the table = size of state space in the environemnt
+#
+# number of columns is equivalent size of action space
 
 # %%
 action_space_size = env.action_space.n
@@ -26,18 +37,27 @@ state_space_size = env.observation_space.n
 q_table = np.zeros((state_space_size, action_space_size))
 print(q_table)
 
+# %% [markdown]
+# ### Initializing Q-learning parameters
+# Now we initalize all the paramenters.
+# `num_episodes` defines the total number of episodes.
+# `learning_rate` is represented with the symbol $\alpha$.
+# `discount_rate` is represented with the symbol $\gama$.
+# `exploration_rate` is represented with the symbol $\epsilon$.
 
 # %%
-num_episodes = 1000
+num_episodes = 10000
+# we define a maximum number of steps that our agent is allowed to take within a single episode
 max_steps_per_episode = 100
 
-learning_rate = 0.01            # Alpha
+# Alpha the weighted for sum of old value and learned value in Q
+learning_rate = 0.01
 discount_rate = 0.99
 
 exploration_rate = 1
 max_exploration_rate = 1
 min_exploration_rate = 0.01
-exploration_decay_rate = 0.01
+exploration_decay_rate = 0.001
 
 # %% [markdown]
 # ###  Implement Reinforcement learning
@@ -48,6 +68,7 @@ rewards_all_episodes = []
 # Q-learning
 for episode in range(num_episodes):
     state = env.reset()
+    print("episodes:", episode)
 
     done = False
     rewards_current_episode = 0  # The start rewards
@@ -55,6 +76,7 @@ for episode in range(num_episodes):
 
         # Exploration-exploitation trade-off
         exploration_rate_threshold = random.uniform(0, 1)
+        # this will determine whether our agent will explore or expoit
         if exploration_rate_threshold > exploration_rate:
             action = np.argmax(q_table[state, :])
         else:
@@ -66,24 +88,25 @@ for episode in range(num_episodes):
         q_table[state, action] = q_table[state, action] * (1-learning_rate) + learning_rate*(
             reward + discount_rate * np.max(q_table[new_state, :]))
 
+        # Transition to the next state
         state = new_state
         rewards_current_episode += reward
 
         if done == True:
             break
 
-        # Exploration rate decay
-        exploration_rate = min_exploration_rate + \
-            (max_exploration_rate - min_exploration_rate) * \
-            np.exp(-exploration_decay_rate * episode)
+    # Exploration rate decay
+    exploration_rate = min_exploration_rate + \
+        (max_exploration_rate - min_exploration_rate) * \
+        np.exp(-exploration_decay_rate * episode)
 
-        rewards_all_episodes.append(rewards_current_episode)
+    rewards_all_episodes.append(rewards_current_episode)
 
+# %%
 # Calculate and print the a erage reward per thousand episodes
 rewards_per_thousand_episodes = np.split(
     np.array(rewards_all_episodes), num_episodes/1000)
 count = 1000
-
 print("*********Average reward per thousand episodes *******\n")
 for r in rewards_per_thousand_episodes:
     print(count, ":", str(sum(r/1000)))
@@ -92,5 +115,40 @@ for r in rewards_per_thousand_episodes:
 print("\n\n**********************Q-table*******************\n")
 print(q_table)
 
+# %% [markdown]
+# ### Watch the agent play the game
 
 # %%
+for episode in range(3):
+    # initialize new episode params
+    state = env.reset()
+    done = False
+    print("*******EPISODE ", episode + 1, "**********\n\n\n\n\n")
+    time.sleep(1)
+    for step in range(max_steps_per_episode):
+        display.clear_output(wait=True)
+        env.render()  # render the current state of the environment to display
+        time.sleep(0.3)
+        # Show the current state of environment on screen
+        # Choose action with highest Q-value for current state
+        # Take new action
+        action = np.argmax(q_table[state, :])
+        new_state, reward, done, info = env.step(action)
+
+        state = new_state
+
+        if done:
+            display.clear_output(wait=True)
+            env.render()
+            if reward == 1:
+                # Agent reached the goak and won episode
+                print("**** You reached the goal****")
+                time.sleep(3)
+                display.clear_output(wait=True)
+            else:
+                # Agent stepped in a hole and lost episode
+                print("***** You fell into a hole****")
+                time.sleep(3)
+                display.clear_output(wait=True)
+            break
+env.close
